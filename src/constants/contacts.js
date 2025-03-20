45c7497f8f8b21b9588c704a4d38a24fdb722376
Marina Strukova <meribsv@gmail.com>
2025-03-20 13:41:05 +0200

    update files

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,5 @@
-export const PATH_DB =
+// yarn generate
+import path from "node:path";
+
+// export const PATH_DB = path.join(process.cwd(), "src", "db", "db-songs.json");
+export const PATH_DB = path.resolve("src", "db", "db.json");
